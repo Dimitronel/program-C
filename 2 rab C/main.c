fd616c98b18a4b95a62d0b10afe31000848771c6
Dimitronel <162284447+Dimitronel@users.noreply.github.com>
2024-03-06 19:33:40 +1000

    Update main.c

--- 2 rab C/main.c
+++ 2 rab C/main.c
@@ -3,18 +3,21 @@
 
 int main(){
  int i, j, n, k; 
- double ar[3][3], m[2][2];
+ double ar[n][n], m[n][n];
  
- printf("Vvedite elem massiva raz? 3x3:\n");
- for (i = 0; i < 3; i++){
-  for (j = 0; j < 3; j++){
+ printf("Vvedite razmer massiva raz? :\n");
+ scanf("%d", &n);
+ 
+ printf("Vvedite elem massiva raz? %dx%d:\n", n, n);
+ for (i = 0; i < n; i++){
+  for (j = 0; j < n; j++){
    scanf("%lf", &ar[i][j]);
   }
  }
  
  double g = 0, p = 0;
- for (i = 0; i < 3; i++){
-  for (j = 0; j < 3; j++){
+ for (i = 0; i < n; i++){
+  for (j = 0; j < n; j++){
    if (i == j){
     g += ar[i][j];}
    if (2 == i+j){
@@ -24,29 +27,32 @@ int main(){
  printf("Summa elementov glavnoy diagonaly: %.1lf\n", g);
  printf("Summa elementov pobochnoy diagonaly?: %.1lf\n", p);
  
- printf("Vvedite elem massiva raz 2x2:\n");
- for (i = 0; i < 2; i++){
-  for (j = 0; j < 2; j++){
-   scanf("%d", &m[i][j]);
+ printf("Vvedite elem massiva raz %dx%d:\n", n, n);
+ for (i = 0; i < n; i++){
+  for (j = 0; j < n; j++){
+   scanf("%lf", &m[i][j]);
   }
  }
  
  printf("Kvadrat matric:\n");
- for (i = 0; i < 2; i++){
-  for (j = 0; j < 2; j++){
-   for (k = 0; k < 2; k++){
-    m[i][j] += m[k][j]  *  m[i][k];
+ for (i = 0; i < n; i++){
+  for (j = 0; j < n; j++){
+   for (k = 0; k < n; k++){
+    m[i][j] += m[k][j]    *    m[i][k];
    }
   }
  }
  
- for (i = 0; i < 2; i++){
-  for (j = 0; j < 2; j++){
-   printf("%d ", m[i][j]);
+ for (i = 0; i < n; i++){
+  for (j = 0; j < n; j++){
+   printf("%lf ", m[i][j]);
   }
   printf("\n");
  }
  
  return 0;
-} 
+}
+
+
+
 
