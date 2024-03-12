4c7020a218c4575f52cb5e1ab71d03f28bf0e44b
Dimitronel <isaec1990@edu.vvsu.ru>
2024-03-12 21:00:36 +1000

    5 rab C
    5 rab C

---
+++ 5 rab C/main.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+double *  *  allocateMatrix(int n) {
+    double *  *  matrix = (double *  * ) malloc(n  *  sizeof(double * ));
+    for (int i = 0; i < n; i++) {
+        matrix[i] = (double * ) malloc(n  *  sizeof(double));
+    }
+    return matrix;
+}
+
+void freeMatrix(double *  *  matrix, int n) {
+    for (int i = 0; i < n; i++) {
+        free(matrix[i]);
+    }
+    free(matrix);
+}
+
+double *  *  calculateMatrixOperation(double *  *  matrix1, double *  *  matrix2, char operation, int n) {
+    double *  *  result = allocateMatrix(n);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            switch (operation) {
+                case '+':
+                    result[i][j] = matrix1[i][j] + matrix2[i][j];
+                    break;
+                case '-':
+                    result[i][j] = matrix1[i][j] - matrix2[i][j];
+                    break;
+                case ' * ':
+                    result[i][j] = matrix1[i][j]  *  matrix2[i][j];
+                    break;
+                default:
+                    printf("Неверный знак операции.\n");
+                    freeMatrix(result, n);
+                    return NULL;
+            }
+        }
+    }
+    return result;
+}
+
+int main() {
+    int n;
+    printf("Введите размерность матрицы: ");
+    scanf("%d", &n);
+
+    double *  *  matrix1 = allocateMatrix(n);
+    double *  *  matrix2 = allocateMatrix(n);
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("Введите элемент матрицы [%d][%d]: ", i, j);
+            scanf("%lf", &matrix1[i][j]);
+            matrix2[i][j] = matrix1[i][j];
+        }
+    }
+
+    char operation;
+    printf("Введите знак операции: ");
+    scanf(" %c", &operation);
+
+    double *  *  result = calculateMatrixOperation(matrix1, matrix2, operation, n);
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%lf ", result[i][j]);
+        }
+        printf("\n");
+    }
+
+    freeMatrix(matrix1, n);
+    freeMatrix(matrix2, n);
+    freeMatrix(result, n);
+
+    return 0;
+}
