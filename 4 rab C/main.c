be0b65ec3fea5cae00d1ba95bfee59bc48e8d68b
Dimitronel <162284447+Dimitronel@users.noreply.github.com>
2024-03-20 19:47:36 +1000

    Update main.c

--- 4 rab C/main.c
+++ 4 rab C/main.c
@@ -1,30 +1,24 @@
-#include <stdio.h>
+#include <stdio.h> 
 #include <math.h> 
+#include "triangle.h" 
 
-double calculatePerimeter(double a, double b, double c) {
-    return a + b + c;
-}
-
-double calculateArea(double a, double b, double c) {
-    double s = (a + b + c) / 2;
-    return sqrt(s  *  (s - a)  *  (s - b)  *  (s - c));
-}
-
-int main() {
-    double a, b, c;
-    printf("Vvod dlin storon: ");
-    scanf("%lf %lf %lf", &a, &b, &c);
-
+int is_triangle(double a, double b, double c) {
     if (a + b <= c || a + c <= b || b + c <= a) {
-        printf("Nerevenstva treyg.\n");
+        return 0;
+    } else {
         return 1;
     }
-
-    double perimeter = calculatePerimeter(a, b, c);
-    double area = calculateArea(a, b, c);
-
-    printf("Pirimetr treyg: %lf\n", perimeter);
-    printf("Ploshad treyg: %lf\n", area);
-
-    return 0;
 }
+
+int main(){ 
+ double a, b, c; 
+ printf("Vvedite storony : "); 
+ scanf("%lf %lf %lf", &a, &b, &c); 
+ if (is_triangle(a, b, c)) { 
+        printf("Perimeter : %.2lf\n", perimeter(a, b, c)); 
+        printf("Square : %.2lf\n", square(a, b, c)); 
+ } else { 
+        printf("Treugolnik  net.\n"); 
+ } 
+}
+
